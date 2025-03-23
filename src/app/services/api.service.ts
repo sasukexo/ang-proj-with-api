899ab60d3d3cde163317e5613413eda72cde83d8
Sasukexo <aswinash1002@gmail.com>
2025-03-23 13:25:57 +0530

    initial steps done

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,12 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-@Injectable({  providedIn: 'root'
-
+@Injectable({
+  providedIn: 'root'
 })
 export class ApiService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient) { }      
 
   getContacts():Observable<any>{
     return this.http.get('http://localhost:4500/contacts');
